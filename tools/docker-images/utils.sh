d22c1ef835b1062c979fbcfcce46a87143690466
ChenXing Yang <20001020ycx@gmail.com>
2026-07-21 09:40:11 -0700

    fix knwon bug

--- tools/docker-images/utils.sh
+++ tools/docker-images/utils.sh
@@ -3,7 +3,13 @@
 remove_temp_file_and_prev_image() {
     rm -f "$temp_iid_file"
 
-    [[ -z "$prev_image_id" || "$prev_image_id" == "$new_image_id" ]] && return
+    if [[ -z "$new_image_id" ]]; then
+        rm -f "$iid_file"
+    elif [[ "$prev_image_id" == "$new_image_id" ]]; then
+        return
+    fi
+
+    [[ -z "$prev_image_id" ]] && return
 
     docker image inspect "$prev_image_id" >/dev/null 2>&1 || return
 
